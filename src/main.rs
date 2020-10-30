039c17831950be6273e2c6b57c317fcd22882e31
Manish Shankla <manish@Manishs-MacBook-Pro-2.local>
2020-10-30 13:21:05 -0700

    tcp stream

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use server::Server;
 
-
-
+mod http;
+mod server;
 
 fn main() {
     let string = String::from("127.0.0.1:8080");
@@ -8,28 +9,7 @@ fn main() {
     server.run();
 }
 
-
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    //Self and server are the same 
-    fn new(addr: String) -> Self {
-        Self {
-            addr
-        }
-    }
-
-
-    fn run(&self) {
-        println!("Listening on {}",self.addr);
-    }
-
-}
-
-
-/* Example request 
+/* Example request
 GET /user?id=10 HTTP/1.1\r\n
 HEADERS \r\n
 BODY
